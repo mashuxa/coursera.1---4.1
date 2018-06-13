8f47b4a7eec78876a68c80730a6dadd23e6efdc8
Masha <mashuxa@tut.by>
2018-06-14 01:10:17 +0300

    done

--- index.js
+++ index.js
@@ -1,26 +1,89 @@
-/**
- * @param {Array} collection
- * @params {Function[]} – Функции для запроса
- * @returns {Array}
- */
-function query(collection) {
+function query() {
 
+
+    var resultArr = arguments[0].slice();
+    var queryParams = {
+        select: [],
+        filter: {}
+    };
+
+
+    if (arguments.length > 0) {
+        for (var i = 1; i < arguments.length; i++) {
+            var propObj = arguments[i];
+
+            if (propObj.propType === "filter") {
+                if (queryParams.filter.hasOwnProperty(propObj.fieldName)) {
+                    queryParams.filter[propObj.fieldName].forEach(function (el, i) {
+                        if (propObj.fieldValues.indexOf(el) === -1) {
+                            queryParams.filter[propObj.fieldName].splice(i, 1);
+                        }
+                    });
+                } else {
+                    queryParams.filter[propObj.fieldName] = propObj.fieldValues;
+                }
+
+
+            } else if (propObj.propType === "select") {
+                if (queryParams.select.length === 0) {
+                    queryParams.select = queryParams.select.concat(propObj.fieldNames);
+                } else {
+                    queryParams.select.forEach(function (el, i) {
+                        if (propObj.fieldNames.indexOf(el) === -1) {
+                            queryParams.select.splice(i, 1);
+                        }
+                    });
+                }
+
+            }
+        }
+    }
+    console.log(resultArr);
+
+    resultArr.forEach(function (obj, i) {
+
+        for (var prop in queryParams.filter) {
+            if (queryParams.filter[prop].indexOf(obj[prop]) === -1) {
+                resultArr.splice(i, 1);
+            }
+        }
+    });
+    resultArr.forEach(function (obj, i) {
+
+
+        for (var key in obj) {
+            if (queryParams.select.indexOf(key) === -1) {
+                delete obj[key];
+            }
+        }
+
+
+    });
+
+
+    return resultArr;
 }
 
-/**
- * @params {String[]}
- */
-function select() {
-
-}
-
-/**
- * @param {String} property – Свойство для фильтрации
- * @param {Array} values – Массив разрешённых значений
- */
 function filterIn(property, values) {
-
+    return {
+        propType: "filter",
+        fieldName: property,
+        fieldValues: values
+    }
 }
+
+function select() {
+    var args = [];
+    for (var i = 0; i < arguments.length; i++) {
+        args[i] = arguments[i];
+    }
+    return {
+        propType: "select",
+        fieldNames: args
+    }
+}
+
+
 
 module.exports = {
     query: query,
